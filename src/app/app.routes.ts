a9170f5786d6824c6a604cc434579f1bb43c792f
Federico Tudini <fede.tudini98@gmail.com>
2024-07-11 17:28:47 +0200

    fix: lazyloading components poeplelist carslist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { CarsListComponent } from './components/cars-list/cars-list.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: 'people', loadComponent: () => import("./components/people-list/people-list.component").then((m) => m.PeopleListComponent)},
+    { path: 'cars', loadComponent: () => import("./components/cars-list/cars-list.component").then((m) => m.CarsListComponent)},
+    { path: '', redirectTo: '/people', pathMatch: 'full' }
+  ];
