307154dad88252b5b6194ea5632091d40215b779
Federico Tudini <fede.tudini98@gmail.com>
2024-07-11 19:24:21 +0200

    feat: people list component

--- src/app/components/people-list/people-list.component.ts
+++ src/app/components/people-list/people-list.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
+import { PeopleService } from '../../services/people.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-people-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './people-list.component.html',
   styleUrl: './people-list.component.scss'
 })
 export class PeopleListComponent {
+
+  data: any[] = [];
+
+  constructor(private peopleService: PeopleService) {
+    effect(() => {
+      this.data = this.peopleService.getPeople()
+      console.log("PEOPLE LIST")
+    })
+  }
+
+  addPerson() {
+    this.peopleService.addPerson({ firstName: "Federico", lastName: "Tudini", age: 26})
+  }
 
 }
