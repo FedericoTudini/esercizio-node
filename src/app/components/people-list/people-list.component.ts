3b21b81f9a65043bcdfab31e93444b0a27e92db2
Federico Tudini <fede.tudini98@gmail.com>
2024-07-13 18:30:14 +0200

    feat: sorting and filtering basic

--- src/app/components/people-list/people-list.component.ts
+++ src/app/components/people-list/people-list.component.ts
@@ -3,7 +3,7 @@ import { PeopleService } from '../../services/people.service';
 import { CommonModule } from '@angular/common';
 import { SearchbarComponent } from '../searchbar/searchbar.component';
 import { Person } from '../../models/person.interface';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-people-list',
@@ -14,14 +14,36 @@ import { Observable } from 'rxjs';
 })
 export class PeopleListComponent {
 
+
   people!: Observable<Person[]>;
+  filteredPeople!: Observable<Person[]>;
 
   constructor(private peopleService: PeopleService) {
     this.people = this.peopleService.people$
+    this.filteredPeople = this.peopleService.people$
   }  
 
   addPerson() {
     this.peopleService.addPerson({ firstName: "Federico", lastName: "Tudini", age: 26})
   }
+
+  onSearch(data: Observable<any[]>) {
+    this.filteredPeople = data
+  }
+
+
+  sortByName(order: string) {
+    this.filteredPeople = this.filteredPeople.pipe(
+      map(arr => arr.sort((a, b) => {
+        return order === 'desc' ? -a.firstName.localeCompare(b.firstName) : a.firstName.localeCompare(b.firstName)
+      }))
+    )
+  }
+    
+    /* sort((a, b) => {
+      if (a.name < b.name) return order === 'asc' ? -1 : 1;
+      if (a.name > b.name) return order === 'asc' ? 1 : -1;
+      return 0;
+    }); */
 
 }
--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -1,9 +1,10 @@
-import { Component, effect, Input, input, OnInit, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, effect, EventEmitter, Input, input, OnInit, Output, Signal, signal, WritableSignal } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Person } from '../../models/person.interface';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-searchbar',
@@ -14,6 +15,29 @@ import { Person } from '../../models/person.interface';
 })
 export class SearchbarComponent {
 
+  @Output() search = new EventEmitter<Observable<any[]>>();
+  @Input() data!: Observable<any[]>;
+  searchString!: string;
+
+  constructor() {
+    this.searchString = "a"
+  }
+
+  filterData() {
+    this.data = this.data.pipe(
+      map((items) => items.filter(item => {
+        let concatFields: string = ''
+        Object.entries(item).forEach(([key, value]) => {
+          concatFields += value
+        });
+        return concatFields.includes(this.searchString)
+      }))
+    )
+    this.search.emit(this.data);
+  }
+
   
 
 }
+
+
